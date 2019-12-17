e3bfe3ad4ecad65a47b5bbfbbda79a9d775ecfa1
Tismoney <mokrov@frtk.ru>
2019-12-17 18:59:56 +0300

    Fix bugs

--- befree/examples/train.py
+++ befree/examples/train.py
@@ -1,6 +1,8 @@
 import torch
 from torch.nn import functional as F
 from ..optimizers.basic_optim import LBFGS
+import numpy as np
+from time import time
 
 ## Will be deleted in the future
 class CustomOptimizer: 
@@ -19,24 +21,26 @@ def train(model, train_loader, optimizer, criterion, metrics, epoch):
     model.train()
     stats = {key: [] for key in ['loss'] + list(metrics.keys())}
     for epoch_i in range(1, epoch + 1):
+        start = time()
         for batch_idx, (inputs, targets) in enumerate(train_loader):
             if torch.cuda.is_available():           
                 inputs, targets = inputs.cuda(), targets.cuda()
+            # inputs.requires_grad = True
 
             if isinstance(optimizer, CustomOptimizer):
                 (loss, predictions) = optimizer.step(model, criterion)
             elif isinstance(optimizer, LBFGS):
                 def closure():
-                    optimizer.zero_grad()
+                    if torch.is_grad_enabled():
+                        optimizer.zero_grad()
                     predictions = model(inputs)
                     loss = criterion(predictions, targets)
-                    loss.backward()
+                    if loss.requires_grad:
+                        loss.backward()
                     return loss
                 loss = optimizer.step(closure)
-                with torch.no_grad(): 
-                    predictions = model(inputs)
-            else:
-                # standard optimizer
+                predictions = model(inputs)
+            else: # standard optimizer
                 optimizer.zero_grad()
                 predictions = model(inputs)
                 loss = criterion(predictions, targets)
@@ -49,8 +53,10 @@ def train(model, train_loader, optimizer, criterion, metrics, epoch):
                     res = func(predictions, targets)
                     stats[name].append(res.item())
 
-        print_stat = f"[{epoch_i}/{epoch}] epoch: Loss - {stats['loss'][-1]:.3f}  "
+        print_stat = f"[{epoch_i}/{epoch}] epoch | Loss: {np.mean(stats['loss'][-25]):.3f} | "
         for name in metrics.keys():
-            print_stat += f"{name} - {stats[name][-1]:.3f} "
+            print_stat += f"{name} : {np.mean(stats[name][-25:]):.3f} | "
+        end = time()
+        print_stat += f" time: {end - start:.2f}s"
         print(print_stat)
     return stats
