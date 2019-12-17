aa74d6a5d2919b1f603f77009ab2c53e0fc4d159
Tismoney <mokrov@frtk.ru>
2019-12-17 14:38:57 +0300

    Upgrade trainer to custom loss and metrics & fix print_stats for new format

--- befree/examples/train.py
+++ befree/examples/train.py
@@ -6,38 +6,51 @@ from ..optimizers.basic_optim import LBFGS
 class CustomOptimizer: 
     None
 
-def train(model, train_loader, optimizer, epoch):
+def train(model, train_loader, optimizer, criterion, metrics, epoch):
+    '''
+        Params:
+            model: pytorch model
+            train_loader: train dataset
+            optimizer: torch.optim.Optimizer
+            criterion: loss function
+            metrics: dict of functions
+            epoch: int, num of epoch
+    '''
     model.train()
-    stats = []
+    stats = {key: [] for key in ['loss'] + list(metrics.keys())}
     for epoch_i in range(1, epoch + 1):
-        for batch_idx, (input, output) in enumerate(train_loader):
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
             if torch.cuda.is_available():           
-                input, output = input.cuda(), output.cuda()
+                inputs, targets = inputs.cuda(), targets.cuda()
 
             if isinstance(optimizer, CustomOptimizer):
-                (loss, predictions) = optimizer.step(model_fn, loss_fn)
+                (loss, predictions) = optimizer.step(model, criterion)
             elif isinstance(optimizer, LBFGS):
                 def closure():
                     optimizer.zero_grad()
-                    predictions = model(input)
-                    loss = F.cross_entropy(predictions, output)
+                    predictions = model(inputs)
+                    loss = criterion(predictions, targets)
                     loss.backward()
                     return loss
                 loss = optimizer.step(closure)
                 with torch.no_grad(): 
-                    predictions = model(input)
+                    predictions = model(inputs)
             else:
                 # standard optimizer
                 optimizer.zero_grad()
-                predictions = model(input)
-                loss = F.cross_entropy(predictions, output)
+                predictions = model(inputs)
+                loss = criterion(predictions, targets)
                 loss.backward()
                 optimizer.step()
 
-            predictions = predictions.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            acc = predictions.eq(output.view_as(predictions)).double().mean()
+            stats['loss'].append(loss.item())
+            with torch.no_grad(): 
+                for name, func in metrics.items():
+                    res = func(predictions, targets)
+                    stats[name].append(res.item())
 
-            stats.append([loss.item(), acc.item()])
-        
-        print(f'[{epoch_i}/{epoch}] epoch: Loss: {stats[-1][0]:.3f}  Accuracy:{stats[-1][1]:.3f}')
+        print_stat = f"[{epoch_i}/{epoch}] epoch: Loss - {stats['loss'][-1]:.3f}  "
+        for name in metrics.keys():
+            print_stat += f"{name} - {stats[name][-1]:.3f} "
+        print(print_stat)
     return stats
